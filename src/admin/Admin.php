a28fb8a6946beaed5c593f5187e11997fcc2821a
James Ellis <james@host.dpcdev>
2018-11-14 03:09:09 +0000

    fix menu icon

--- src/admin/Admin.php
+++ src/admin/Admin.php
@@ -9,11 +9,7 @@ use SilverStripe\Admin\ModelAdmin;
 class CspModelAdmin extends ModelAdmin {
   private static $url_segment = 'content-security-policy';
   private static $menu_title = 'CSP';
-  /**
-   * Menu icon for Left and Main CMS
-   * @var string
-   */
-  private static $menu_icon = '/framework/admin/images/menu-icons/16x16/gears.png';
+  private static $menu_icon_class = 'font-icon-block';
   private static $managed_models = [
     Policy::class,
     ViolationReport::class
