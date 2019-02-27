13553ba5fbf585aba72df9d9430ad96e2799336e
James Ellis <james@host.dpcdev>
2019-02-27 04:15:40 +0000

    Update count in job to be correct

--- src/jobs/PruneViolationReportsJob.php
+++ src/jobs/PruneViolationReportsJob.php
@@ -61,7 +61,7 @@ class PruneViolationReportsJob extends AbstractQueuedJob {
     //SS_Log::log($message, SS_Log::DEBUG);
     $this->addMessage($message);
 
-    $this->totalSteps = $this->currentStep = $post_count;
+    $this->totalSteps = $this->currentStep = $post_count - $pre_count;
 
     $this->isComplete = true;
     return;
