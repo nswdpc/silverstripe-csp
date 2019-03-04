d54921b3e2ce9d1db5fa93b15f70b4c7a9e78361
James Ellis <james@host.dpcdev>
2019-03-04 04:52:30 +0000

    Add an extension and trait that can be used on demand to enable CSP delivery on those controllers

---
+++ src/traits/ContentSecurityPolicyAware.php
@@ -0,0 +1,11 @@
+<?php
+namespace NSWDPC\Utilities\ContentSecurityPolicy;
+/**
+ * Use this trait to enable the CSP on non ContentController controllers
+ * @author James
+ */
+trait ContentSecurityPolicyAware {
+    public function EnableContentSecurityPolicy() {
+        return true;
+    }
+}
